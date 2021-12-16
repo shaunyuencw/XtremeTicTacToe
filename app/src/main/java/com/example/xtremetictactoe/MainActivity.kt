2b388df601e111dbc2faa6df00af30f739b28cb4
Shaun Yuen <shaunyuencw@gmail.com>
2021-12-16 18:23:45 +0800

    Google Sign in Supported

--- app/src/main/java/com/example/xtremetictactoe/MainActivity.kt
+++ app/src/main/java/com/example/xtremetictactoe/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.example.xtremetictactoe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private companion object {
+        private const val TAG = "MainActivity"
+    }
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+
+        Toast.makeText(this, "MainActivity", Toast.LENGTH_SHORT)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.miLogout) {
+            Log.i(TAG, "Logout")
+
+            // Logout the user
+            auth.signOut()
+
+            val logoutIntent = Intent(this, LoginActivity::class.java)
+            logoutIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+
+            startActivity(logoutIntent)
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
