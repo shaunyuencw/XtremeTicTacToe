da0a0d7e55864d12a390cea4a3381fc15e1df9a9
Shaun Yuen <shaunyuencw@gmail.com>
2021-12-16 20:05:47 +0800

    Basic Read Functionality (TBC)

--- app/src/main/java/com/example/xtremetictactoe/MainActivity.kt
+++ app/src/main/java/com/example/xtremetictactoe/MainActivity.kt
@@ -4,20 +4,36 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
+import android.view.*
+import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import org.w3c.dom.Text
+
+data class User(
+    val displayName: String = "",
+    val emojis: String = ""
+)
+
+class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
 class MainActivity : AppCompatActivity() {
-
     private companion object {
         private const val TAG = "MainActivity"
     }
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var rvUsers: RecyclerView
+
+    // Access a Cloud Firestore instance from your Activity
+    val db = Firebase.firestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +41,28 @@ class MainActivity : AppCompatActivity() {
 
         auth = Firebase.auth
 
+        rvUsers = findViewById(R.id.rvUsers)
+
+        val query = db.collection("users")
+        val options = FirestoreRecyclerOptions.Builder<User>().setQuery(query, User::class.java)
+            .setLifecycleOwner(this).build()
+
+        val adapter = object: FirestoreRecyclerAdapter<User, UserViewHolder>(options) {
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+                val view = LayoutInflater.from(this@MainActivity).inflate(android.R.layout.simple_list_item_2, parent, false)
+                return UserViewHolder(view)
+            }
+
+            override fun onBindViewHolder(holder: UserViewHolder, position: Int, model: User) {
+                val tvName: TextView = holder.itemView.findViewById(android.R.id.text1)
+                val tvEmojis: TextView = holder.itemView.findViewById(android.R.id.text2)
+
+                tvName.text = model.displayName
+                tvEmojis.text = model.emojis
+            }
+        }
+        rvUsers.adapter = adapter
+        rvUsers.layoutManager = LinearLayoutManager(this)
         Toast.makeText(this, "MainActivity", Toast.LENGTH_SHORT)
     }
 
